d94bd9da7e4f8af9ca0c7b25be6d7fcc263dcb01
moses <mlee@mozeus.com>
2022-07-19 13:00:23 -0400

    Delete bookmarks

--- script.js
+++ script.js
@@ -22,8 +22,6 @@ const showModal = () => {
 const closeModal = (e) => {
   if (e.target === modal || e.target === modalClose) {
     modal.classList.remove("show-modal");
-    // websiteNameEl.value = "";
-    // websiteUrlEl.value = "";
   }
 };
 
@@ -46,6 +44,42 @@ const validate = (nameValue, urlValue) => {
   return true;
 };
 
+// Build Bookmarks
+const buildBookmarks = () => {
+  // Clear bookmarksContainer's elements
+  bookmarksContainer.textContent = "";
+  // Build items
+  bookmarks.forEach((bookmark) => {
+    const { name, url } = bookmark;
+    // Item
+    const item = document.createElement("div");
+    item.classList.add("item");
+    // Close Icon
+    const closeIcon = document.createElement("i");
+    closeIcon.classList.add("fas", "fa-times");
+    closeIcon.setAttribute("title", "Delete Bookmark");
+    closeIcon.setAttribute("onclick", `deleteBookmark('${url}')`);
+    // Favicon / Link Container
+    const linkInfo = document.createElement("div");
+    linkInfo.classList.add("name");
+    // Favicon
+    const favicon = document.createElement("img");
+    favicon.setAttribute(
+      "src",
+      `https://s2.googleusercontent.com/s2/favicons?domain=${url}`
+    );
+    favicon.setAttribute("alt", "Favicon");
+    const link = document.createElement("a");
+    link.setAttribute("href", `${url}`);
+    link.setAttribute("target", "_blank");
+    link.textContent = name;
+    // Append to bookmark's container
+    linkInfo.append(favicon, link);
+    item.append(closeIcon, linkInfo);
+    bookmarksContainer.appendChild(item);
+  });
+};
+
 // Fetch Bookmarks
 const fetchBookmarks = () => {
   // Get bookmarks from localStorage if available
@@ -55,12 +89,25 @@ const fetchBookmarks = () => {
     // Create bookmarks array in localStorage
     bookmarks = [
       {
-        name: "Jacinto Design",
-        url: "https://jacinto.design",
+        name: "moses lee | dev",
+        url: "https://moseslee.dev",
       },
     ];
     localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
   }
+  buildBookmarks();
+};
+
+// Delete Bookmark
+const deleteBookmark = (url) => {
+  bookmarks.forEach((bookmark, i) => {
+    if (bookmark.url === url) {
+      bookmarks.splice(i, 1);
+    }
+  });
+  // Update bookmarks array in localStorage, re-pop DOM
+  localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
+  fetchBookmarks();
 };
 
 // Handle Data from Form
